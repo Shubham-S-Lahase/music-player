a29ce835ca04f68d2f080cba11e9341316182990
Shubham Lahase <shubhamlahase@gmail.com>
2024-08-13 10:04:39 +0530

    drawer menu added for small devices

--- src/App.js
+++ src/App.js
@@ -3,24 +3,42 @@ import Sidebar from "./sidebar/Sidebar";
 import Player from "./player/Player";
 import Playercard from "./molecules/playercard/Playercard";
 import { MusicProvider } from "./context";
+import { useState } from "react";
 
 function App() {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className="App">
       <MusicProvider>
-      <div className="container">
-        <div className="sidenav">
-          <Sidebar/>
-        </div>
-        <div className="mainarea">
-          <div className="player">
-            <Player/>
+        <div className="container">
+          <div className={`sidenav ${isSidebarOpen ? "open" : ""}`}>
+            <Sidebar />
           </div>
-          <div className="sidemore">
-            <Playercard/>
+          <div className="mainarea">
+            <div className="player">
+              <Player />
+            </div>
+            <div className="sidemore">
+              <Playercard />
+            </div>
           </div>
+          <div
+            className={`overlay ${isSidebarOpen ? "active" : ""}`}
+            onClick={toggleSidebar}
+          ></div>
+          <button
+            className="menu-toggle"
+            onClick={toggleSidebar}
+            aria-label="Toggle Menu"
+          >
+            ☰
+          </button>
         </div>
-      </div>
       </MusicProvider>
     </div>
   );
